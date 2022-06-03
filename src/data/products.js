82f04ef0994aaba89ec371f113fea1b166a51706
vladislav <vladislav.b@affetta.ru>
2022-06-03 14:59:33 +0300

    init

---
+++ src/data/products.js
@@ -0,0 +1,75 @@
+const products = [
+  {
+    id: 1,
+    title: 'Iphone 13 mini',
+    price: 70000,
+    image: 'https://htstatic.imgsmail.ru/pic_original/2753a54de89d8e2a21ad9836b9c9b590/2000907/'
+  },
+  {
+    id: 2,
+    title: 'Iphone 13 mini',
+    price: 70000,
+    image: 'https://htstatic.imgsmail.ru/pic_original/2753a54de89d8e2a21ad9836b9c9b590/2000907/'
+  },
+  {
+    id: 3,
+    title: 'Iphone 13 mini',
+    price: 70000,
+    image: 'https://htstatic.imgsmail.ru/pic_original/2753a54de89d8e2a21ad9836b9c9b590/2000907/'
+  },
+  {
+    id: 4,
+    title: 'Iphone 13 mini',
+    price: 70000,
+    image: 'https://htstatic.imgsmail.ru/pic_original/2753a54de89d8e2a21ad9836b9c9b590/2000907/'
+  },
+  {
+    id: 5,
+    title: 'Iphone 13 mini',
+    price: 70000,
+    image: 'https://htstatic.imgsmail.ru/pic_original/2753a54de89d8e2a21ad9836b9c9b590/2000907/'
+  },
+  {
+    id: 6,
+    title: 'Iphone 13 mini',
+    price: 70000,
+    image: 'https://htstatic.imgsmail.ru/pic_original/2753a54de89d8e2a21ad9836b9c9b590/2000907/'
+  },
+  {
+    id: 7,
+    title: 'Iphone 13 mini',
+    price: 70000,
+    image: 'https://htstatic.imgsmail.ru/pic_original/2753a54de89d8e2a21ad9836b9c9b590/2000907/'
+  },
+  {
+    id: 8,
+    title: 'Iphone 13 mini',
+    price: 70000,
+    image: 'https://htstatic.imgsmail.ru/pic_original/2753a54de89d8e2a21ad9836b9c9b590/2000907/'
+  },
+  {
+    id: 9,
+    title: 'Iphone 13 mini',
+    price: 70000,
+    image: 'https://htstatic.imgsmail.ru/pic_original/2753a54de89d8e2a21ad9836b9c9b590/2000907/'
+  },
+  {
+    id: 10,
+    title: 'Iphone 13 mini',
+    price: 70000,
+    image: 'https://htstatic.imgsmail.ru/pic_original/2753a54de89d8e2a21ad9836b9c9b590/2000907/'
+  },
+  {
+    id: 11,
+    title: 'Iphone 13 mini',
+    price: 70000,
+    image: 'https://htstatic.imgsmail.ru/pic_original/2753a54de89d8e2a21ad9836b9c9b590/2000907/'
+  },
+  {
+    id: 12,
+    title: 'Iphone 13 mini',
+    price: 70000,
+    image: 'https://htstatic.imgsmail.ru/pic_original/2753a54de89d8e2a21ad9836b9c9b590/2000907/'
+  },
+]
+export default products
---
+++ src/views/Catalog.vue
@@ -0,0 +1,18 @@
+<template>
+  <ProductList :products="products" />
+</template>
+<script>
+import ProductList from "@/components/Products/ProductList.vue";
+import products from "@/data/products";
+export default {
+  name: "Catalog",
+  components: {
+    ProductList,
+  },
+  data() {
+    return {
+      products,
+    };
+  },
+};
+</script>
